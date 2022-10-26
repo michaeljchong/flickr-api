ebd3179f6784d5e71c04ffece175c2b0155cf083
Michael Chong <mikec0920@gmail.com>
2022-10-25 23:12:19 -0700

    Create search variables

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    @flickr = Flickr.new
+    if params[:user_id].present?
+      @photos = @flickr.photos.search user_id: params[:user_id]
+    end
   end
 end
